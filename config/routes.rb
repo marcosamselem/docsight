bcda58d35dc589bb9e30e81b6e82e8a423cb0f2f
Marcos Amselem <marcosamselem27@gmail.com>
2023-08-29 16:58:52 +0100

    skelleton controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   devise_for :doctors
   devise_for :users
   root to: "pages#home"
+  resources :doctors do
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
