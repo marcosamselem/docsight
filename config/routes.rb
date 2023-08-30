2b85b3e3141954baa0a80a298454d1e08ef5e78c
David Whitehead <dejwhitehead@protonmail.com>
2023-08-30 11:33:26 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :doctors
   devise_for :users
   root to: "pages#home"
-  resources :doctors [:index]
+  resources :doctors, only: [:index] 
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
