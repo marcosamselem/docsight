a75bc8f34aa69f2f817e4adf980c889ff6f77c2d
Miguel Belo <belo_miguel@hotmail.com>
2023-08-31 17:28:43 +0100

    Adding actions to Appointments controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,28 @@
 class AppointmentsController < ApplicationController
+  before_action :set_doctor, only: [:new, :create]
+
+
+  def new
+    @appointment = Appointment.new
+  end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+    @appointment.doctor = @doctor
+    @appointment.patient = current_user
+    raise
+    # @appointment.patient = @user
+    # @appointment.doctor = @doctor
+  end
+
+  private
+
+  def set_doctor
+    @doctor = User.find(params[:user_id])
+  end
+
+  def appointment_params
+    params.require(:appointment).permit(:symptoms, :start_date, :end_date)
+  end
+
 end
