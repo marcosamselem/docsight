43ccdcc21427d855ba0e64d652d61d61a8d48a97
Marcos Amselem <marcosamselem27@gmail.com>
2023-08-31 17:16:01 +0100

    devise adjusted to new and create methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,22 @@ class UsersController < ApplicationController
   def index
     @doctors = User.where(role: "doctor")
   end
+
+  def new
+    @patient = User.new
+  end
+
+  def create
+    @patient = User.new(params_user)
+    @patient = User.find(params[:id])
+    @patient.save!
+    redirect_to users_path
+  end
+
+  private
+
+  def params_user
+    params.require(:user).permit(:first_name, :last_name, :bio, :email, :password, :phone_number, :specialty)
+  end
 end
+ 
