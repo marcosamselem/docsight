7b48417589cd6b7124306e19bae9a929e2aa5b78
Marcos Amselem <marcosamselem27@gmail.com>
2023-08-31 14:24:15 +0100

    add validations and update seed

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,5 @@
 class Location < ApplicationRecord
+  has_many :doctors_locations, dependent: :destroy
   has_many :users, through: :doctors_locations
   validates :name, presence: true
   validates :address, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,15 @@ class User < ApplicationRecord
 
   has_many :qualifications
   has_many :procedures
+  has_many :doctors_locations, dependent: :destroy
   has_many :locations, through: :doctors_locations
   has_many :appointments_as_patient, class_name: "Appointment", foreign_key: :patient_id
   has_many :appointments_as_doctor, class_name: "Appointment", foreign_key: :doctor_id
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :phone_number, presence: true
-  validates :date_of_birth, presence: true
-  validates :gender, presence: true
+  validates :date_of_birth, presence: true, if: :user_is_patient?
+  validates :gender, presence: true, if: :user_is_patient?
   validates :role, presence: true
   validates :past_interventions, presence: true, if: :user_is_patient?
 
