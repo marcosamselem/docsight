f2f9196c1434ac641d347f76e071aa8ad843d69a
yusuf celikdemir <urasit62@gmail.com>
2023-09-04 11:14:02 +0100

    DO not merge just a test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 require "faker"
+# Test comment
 
 puts "Cleaning all DB's (Locations, Procedures and Users)"
 Procedure.destroy_all
