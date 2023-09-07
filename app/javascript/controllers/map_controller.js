2d9f138ddf52430200782d46a747dea5ee0d6c29
Marcos Amselem <marcosamselem27@gmail.com>
2023-09-07 16:55:45 +0100

    all ready for deploy

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -25,9 +25,7 @@ export default class extends Controller {
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
-      const customMarker = document.createElement("div")
-      customMarker.innerHTML = marker.marker_html
-      new mapboxgl.Marker(customMarker)
+      new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
         .setPopup(popup)
         .addTo(this.map)
